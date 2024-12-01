d057c963197769c1b4a7b62def072519e0a6cf3d
eduEsron <141090507+eduEsron@users.noreply.github.com>
2024-12-01 16:09:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let SignUpBtn = document.getElementById("SignUpBtn")
 let UserName_Login = document.getElementById("Username")
 let Password_Login = document.getElementById("Password")
 let LoginBtn = document.getElementById("loginBtn")
+let username = document.getElementById("user_name")
+
 if(SignUpBtn){
     SignUpBtn.addEventListener("click", function(){
     if(UserName_SignUp.value == "" || Password_SignUp.value == "" || Password_SignUp2.value == ""){
@@ -30,18 +32,49 @@ if(LoginBtn){
     let storedPass = localStorage.getItem("Password")
     if(UserName_Login.value == storedName && storedPass == storedPass){
         window.location.href = "./main.html"
+        username.textContent = storedName.value
     }
 })
 }
-//pagina create
+//pagina main
 let newName = document.getElementById("newName")
-let newCategoria = document.getElementById("createCategoria")
+let newCategoria = document.getElementById("createCategoria") 
 let newData = document.getElementById("newData")
 let newValue = document.getElementById("newValor")
 let CreateBtn = document.getElementById("CreateFinanca")
 let newDesc = document.getElementById("newDesc")
+let finances = JSON.parse(localStorage.getItem("finances")) || []
+function saveFinances() {
+    localStorage.setItem("finances", JSON.stringify(finances))
+}
+function CriarFinancas(){
+  let dados = {
+        name: newName.value,
+        categoria: newCategoria.value,
+        data: newData.value,
+        valor: newValue.value,
+        descricao: newDesc.value
+    }
+    finances.push(dados)
+    saveFinances()
+    RenderFinancas(dados)
+}
+function RenderFinancas(dados){
+    let MainM = document.getElementById("Main_M")
+    let createExibirDiv = document.createElement("span")
+    createExibirDiv.innerHTML = `
+    <p>Name: ${dados.name}</p>
+    <p>Categoria: ${dados.categoria}</p>
+    <p>Data: ${dados.data}</p>
+    <p>Valor: ${dados.valor}</p>
+    <p>Descrição: ${dados.descricao}</p>
+    <span><button id="Edit">Editar</button><button id="Delete">Deletar</button></span>
+    `
+    MainM.appendChild(createExibirDiv)
+}
 if(CreateBtn){
-    CreateBtn.addEventListener("click", () =>{
-        localStorage.setItem("newName", newName.value)
-    })
+    for(const f of finances) {
+        RenderFinancas(f)
+    }
+    CreateBtn.addEventListener("click", CriarFinancas)
 }
